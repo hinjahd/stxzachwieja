677e276600fde051338a9cf389f8fbbda3e05140
MateuszZachwieja <mateusz.zachwieja99@gmail.com>
2021-11-29 16:50:24 +0100

    2nd

--- ZachwiejaSTX/settings.py
+++ ZachwiejaSTX/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
